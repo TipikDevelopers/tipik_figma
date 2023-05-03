600dbfee87b6cf6723473545674cbab914c8b003
oykudemir <oykuudemirr16@gmail.com>
2023-05-04 00:44:11 +0300

    kart ekranı

--- lib/creditCard/myCards.dart
+++ lib/creditCard/myCards.dart
@@ -10,6 +10,17 @@ class MyCards extends StatefulWidget {
 }
 
 class _MyCardsState extends State<MyCards> {
+  List<Widget> cardList = [
+    CreditCard(
+      color: Color.fromRGBO(118, 177, 135, 1),
+      cardName: "  Ziraat Gold",
+    ),
+    CreditCard(color: Colors.red, cardName: "  İş Bankası"),
+    CreditCard(
+      color: Colors.blue,
+      cardName: "  Maximum",
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
@@ -39,16 +50,76 @@ class _MyCardsState extends State<MyCards> {
           ),
         ),
         body: Container(
+            margin: EdgeInsets.only(top: 50),
+            width: screenWidth,
+            height: 200,
             child: Swiper(
-          itemHeight: 30,
-          itemBuilder: (BuildContext context, int index) {
-            return new Image.network(
-              "http://via.placeholder.com/288x188",
-            );
-          },
-          itemCount: 10,
-          viewportFraction: 0.8,
-          scale: 0.9,
-        )));
+              itemWidth: screenWidth - 30,
+              itemBuilder: (BuildContext context, int index) {
+                return cardList[index];
+              },
+              itemCount: cardList.length,
+              viewportFraction: 0.7,
+              scale: 0.5,
+            )));
+  }
+}
+
+class CreditCard extends StatelessWidget {
+  final Color color;
+  final String cardName;
+
+  const CreditCard({Key? key, required this.color, required this.cardName})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //BURAYA TEXTVIEWLAR VS DE EKLENEBİLİR, BÖYLECE HER KART İÇİN İNPUT GİRME KISMI AÇILIR
+    return Stack(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(15.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 2,
+                blurRadius: 5,
+                offset: Offset(0, 3),
+              ),
+            ],
+          ),
+          // Add your child widget here
+        ),
+        Positioned(
+          top: 0,
+          left: 0,
+          right: 0,
+          child: Container(
+            height: 45,
+            color: Colors.white,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  cardName,
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16.0,
+                      fontFamily: 'Quicksand',
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
