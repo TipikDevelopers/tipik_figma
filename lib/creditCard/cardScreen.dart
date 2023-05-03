fae26a3356a4fb3c7882c9398f4866b1cf0b6c7b
oykudemir <oykuudemirr16@gmail.com>
2023-05-03 17:07:38 +0300

    update

--- lib/creditCard/cardScreen.dart
+++ lib/creditCard/cardScreen.dart
@@ -40,50 +40,55 @@ class cardScreen extends StatelessWidget {
         color: const Color.fromRGBO(235, 235, 235, 1),
         child: Stack(alignment: Alignment.center, children: <Widget>[
           Positioned(
-            top: 50,
-            height: 200,
-            width: 308,
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              elevation: 15,
-              child: ClipPath(
-                clipper: ShapeBorderClipper(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15))),
-                child: Container(
-                    height: 210,
-                    width: 358,
-                    decoration: const BoxDecoration(
-                        color: Color.fromRGBO(163, 163, 163, 0.4)),
-                    padding: const EdgeInsets.all(20.0),
-                    alignment: Alignment.centerLeft,
-                    child: Container()),
-              ),
-            ),
-          ),
+              top: 50,
+              height: 200,
+              width: 308,
+              child: Opacity(
+                opacity: 0.5,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  elevation: 15,
+                  child: ClipPath(
+                    clipper: ShapeBorderClipper(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15))),
+                    child: Container(
+                        height: 210,
+                        width: 358,
+                        decoration: const BoxDecoration(
+                            color: Color.fromRGBO(163, 163, 163, 0.4)),
+                        padding: const EdgeInsets.all(20.0),
+                        alignment: Alignment.centerLeft,
+                        child: Container()),
+                  ),
+                ),
+              )),
           Positioned(
             top: 80,
             height: 200,
             width: 342,
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              elevation: 15,
-              child: ClipPath(
-                clipper: ShapeBorderClipper(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15))),
-                child: Container(
-                    height: 210,
-                    width: 358,
-                    decoration: const BoxDecoration(
-                        color: Color.fromRGBO(163, 163, 163, 0.65)),
-                    padding: const EdgeInsets.all(20.0),
-                    alignment: Alignment.centerLeft,
-                    child: Container()),
+            child: Opacity(
+              opacity: 0.5,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                elevation: 15,
+                child: ClipPath(
+                  clipper: ShapeBorderClipper(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15))),
+                  child: Container(
+                      height: 210,
+                      width: 358,
+                      decoration: const BoxDecoration(
+                          color: Color.fromRGBO(163, 163, 163, 0.65)),
+                      padding: const EdgeInsets.all(20.0),
+                      alignment: Alignment.centerLeft,
+                      child: Container()),
+                ),
               ),
             ),
           ),
@@ -166,6 +171,144 @@ class cardScreen extends StatelessWidget {
             width: 126,
             height: 58,
             child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromRGBO(86, 194, 95, 1),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(22.0),
+                ),
+                elevation: 5, // Set the elevation to 5
+              ),
+              onPressed: () {
+                if (cardList.isNotEmpty) {
+                  showModalBottomSheet<void>(
+                      context: context,
+                      isScrollControlled: true,
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.vertical(
+                          top: Radius.circular(40.0),
+                        ),
+                      ),
+                      builder: (BuildContext context) {
+                        return Container(
+                          height: MediaQuery.of(context).size.height * 0.65,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Container(
+                                margin:
+                                    const EdgeInsets.only(top: 30, bottom: 10),
+                                child: const Image(
+                                  width: 268,
+                                  height: 61,
+                                  image: NetworkImage(
+                                      'https://www.masterpassturkiye.com/files/01.png'),
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Container(
+                                margin: const EdgeInsets.only(
+                                  left: 40,
+                                  right: 40,
+                                ),
+                                child: const Text(
+                                  'Kayıtlı telefon numaranıza ait Masterpass hesabı bulunmaktadır.',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontFamily: 'Quicksand',
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 20),
+                                ),
+                              ),
+                              const SizedBox(height: 50),
+                              Container(
+                                height: 75,
+                                width: 198,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(22.0),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(0.5),
+                                      spreadRadius: 2,
+                                      blurRadius: 5,
+                                      offset: Offset(0, 3),
+                                    ),
+                                  ],
+                                ),
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor:
+                                        Color.fromRGBO(86, 194, 95, 1),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(22.0),
+                                    ),
+                                  ),
+                                  onPressed: () => Navigator.pop(context),
+                                  child: Padding(
+                                    padding: EdgeInsets.all(10.0),
+                                    child: Text(
+                                      'Kullan',
+                                      style: TextStyle(
+                                        fontSize: 30,
+                                        fontFamily: 'Quicksand',
+                                        fontWeight: FontWeight.w700,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 10),
+                              Container(
+                                margin: const EdgeInsets.only(
+                                  top: 35,
+                                  left: 25,
+                                  right: 25,
+                                ),
+                                child: const Text(
+                                  'Telefon numaranıza kayıtlı kredi kartlarını uygulamaya aktarmak için Kullan butonuna basın.',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontFamily: 'Quicksand',
+                                      fontWeight: FontWeight.normal,
+                                      fontSize: 15),
+                                ),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(
+                                  top: 15,
+                                  left: 25,
+                                  right: 25,
+                                ),
+                                child: const Text(
+                                  'Tipik, kredi kartı bilgilerinize sahip değildir. Kart bilgileriniz, MasterPass altyapısı üzerinden şifreli ve kısıtlı biçimde aktarılır.',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontFamily: 'Quicksand',
+                                      fontWeight: FontWeight.normal,
+                                      fontSize: 15),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      });
+                }
+                //masterpass bulundu
+                else {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => MyCardsNoMasterpass()));
+                }
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(Icons.east, size: 40),
+                ],
+              ),
+            ),
+            /*child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   backgroundColor: const Color.fromRGBO(86, 194, 95, 1),
                   shape: RoundedRectangleBorder(
@@ -299,7 +442,7 @@ class cardScreen extends StatelessWidget {
                         MaterialPageRoute(
                             builder: (context) => MyCardsNoMasterpass()));
                   }
-                }),
+                }),*/
           ),
         ]),
       ),
--- lib/creditCard/myCardsNoMasterpass.dart
+++ lib/creditCard/myCardsNoMasterpass.dart
@@ -35,182 +35,238 @@ class MyCardsNoMasterpass extends StatelessWidget {
             centerTitle: true,
           ),
         ),
-        body: Container(
-          padding: EdgeInsets.only(top: 15),
-          height: MediaQuery.of(context).size.height - 2,
-          width: MediaQuery.of(context).size.width,
-          color: Color.fromRGBO(235, 235, 235, 1),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Center(
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    elevation: 15,
-                    child: ClipPath(
-                      clipper: ShapeBorderClipper(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15))),
-                      child: Container(
-                          height: 210,
-                          width: 358,
-                          decoration: BoxDecoration(
-                              color: Color.fromRGBO(163, 163, 163, 1)),
-                          alignment: Alignment.center,
-                          child: Container()),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.baseline,
-                  textBaseline: TextBaseline.alphabetic,
-                  children: [
-                    CustomPaint(
-                      painter: LinePainter(),
-                    ),
-                    Container(
-                      height: 25,
-                      width: 163,
-                      margin: EdgeInsets.only(left: 125, top: 10, bottom: 10),
-                      child: Text(
-                        'Yeni kart tanımla',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 20.0,
-                            fontFamily: 'Quicksand',
-                            fontWeight: FontWeight.bold),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.only(top: 15),
+            height: MediaQuery.of(context).size.height - 2,
+            width: MediaQuery.of(context).size.width,
+            color: Color.fromRGBO(235, 235, 235, 1),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Center(
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      elevation: 15,
+                      child: ClipPath(
+                        clipper: ShapeBorderClipper(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15))),
+                        child: Container(
+                            height: 210,
+                            width: 358,
+                            decoration: BoxDecoration(
+                                color: Color.fromRGBO(163, 163, 163, 1)),
+                            alignment: Alignment.center,
+                            child: Container()),
                       ),
                     ),
-                  ],
-                ),
-                Container(
-                  height: 25,
-                  width: 272,
-                  margin: EdgeInsets.only(
-                    top: 20,
-                    left: 20,
                   ),
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    'Kart takma adı (Örn: Kişisel Kartım)',
-                    textAlign: TextAlign.start,
-                    style: TextStyle(
-                        color: Color.fromRGBO(90, 89, 89, 1),
-                        fontSize: 16.0,
-                        fontFamily: 'Quicksand',
-                        fontWeight: FontWeight.bold),
+                  SizedBox(
+                    height: 10,
                   ),
-                ),
-                Container(
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: [
+                      CustomPaint(
+                        painter: LinePainter(),
+                      ),
+                      Container(
+                        height: 25,
+                        width: 163,
+                        margin: EdgeInsets.only(left: 125, top: 10, bottom: 10),
+                        child: Text(
+                          'Yeni kart tanımla',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20.0,
+                              fontFamily: 'Quicksand',
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
                     height: 25,
-                    width: 358,
+                    width: 272,
                     margin: EdgeInsets.only(
                       top: 20,
                       left: 20,
                     ),
                     alignment: Alignment.topLeft,
-                    child: TextField(
-                      controller: cardNickname,
-                      decoration: InputDecoration(),
-                      onTapOutside: (event) =>
-                          FocusManager.instance.primaryFocus?.unfocus(),
-                    )),
-                Container(
-                  height: 25,
-                  width: 272,
-                  margin: EdgeInsets.only(
-                    top: 20,
-                    left: 20,
+                    child: Text(
+                      'Kart takma adı (Örn: Kişisel Kartım)',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                          color: Color.fromRGBO(90, 89, 89, 1),
+                          fontSize: 16.0,
+                          fontFamily: 'Quicksand',
+                          fontWeight: FontWeight.bold),
+                    ),
                   ),
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    'Kart numarası',
-                    textAlign: TextAlign.start,
-                    style: TextStyle(
-                        color: Color.fromRGBO(90, 89, 89, 1),
-                        fontSize: 16.0,
-                        fontFamily: 'Quicksand',
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Container(
+                  Container(
+                      height: 25,
+                      width: 358,
+                      margin: EdgeInsets.only(
+                        top: 20,
+                        left: 20,
+                      ),
+                      alignment: Alignment.topLeft,
+                      child: TextField(
+                        controller: cardNickname,
+                        decoration: InputDecoration(),
+                        onTapOutside: (event) =>
+                            FocusManager.instance.primaryFocus?.unfocus(),
+                      )),
+                  Container(
                     height: 25,
-                    width: 358,
+                    width: 272,
                     margin: EdgeInsets.only(
                       top: 20,
                       left: 20,
                     ),
                     alignment: Alignment.topLeft,
-                    child: TextField(
-                      controller: cardNumber,
-                      decoration: InputDecoration(),
-                      onTapOutside: (event) =>
-                          FocusManager.instance.primaryFocus?.unfocus(),
-                    )),
-                Container(
-                  height: 25,
-                  width: 272,
-                  margin: EdgeInsets.only(
-                    top: 20,
-                    left: 20,
+                    child: Text(
+                      'Kart numarası',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                          color: Color.fromRGBO(90, 89, 89, 1),
+                          fontSize: 16.0,
+                          fontFamily: 'Quicksand',
+                          fontWeight: FontWeight.bold),
+                    ),
                   ),
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    'Kart son kullanma tarihi (AA/YY)',
-                    textAlign: TextAlign.start,
-                    style: TextStyle(
-                        color: Color.fromRGBO(90, 89, 89, 1),
-                        fontSize: 16.0,
-                        fontFamily: 'Quicksand',
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Container(
+                  Container(
+                      height: 25,
+                      width: 358,
+                      margin: EdgeInsets.only(
+                        top: 20,
+                        left: 20,
+                      ),
+                      alignment: Alignment.topLeft,
+                      child: TextField(
+                        controller: cardNumber,
+                        decoration: InputDecoration(),
+                        onTapOutside: (event) =>
+                            FocusManager.instance.primaryFocus?.unfocus(),
+                      )),
+                  Container(
                     height: 25,
-                    width: 358,
+                    width: 272,
                     margin: EdgeInsets.only(
                       top: 20,
                       left: 20,
                     ),
                     alignment: Alignment.topLeft,
-                    child: TextField(
-                      controller: cardLastUsage,
-                      decoration: InputDecoration(),
-                      onTapOutside: (event) =>
-                          FocusManager.instance.primaryFocus?.unfocus(),
-                    )),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  height: 72,
-                  width: 323,
-                  margin: EdgeInsets.only(
-                    top: 20,
-                    left: 20,
+                    child: Text(
+                      'Kart son kullanma tarihi (AA/YY)',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                          color: Color.fromRGBO(90, 89, 89, 1),
+                          fontSize: 16.0,
+                          fontFamily: 'Quicksand',
+                          fontWeight: FontWeight.bold),
+                    ),
                   ),
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    'Kart bilgileriniz Masterpass altyapısında saklanır. Kredi kartınızı tanımlanamanız halinde, telefon numaranıza ait bir MasterPass hesabı oluşturulur. Kart bilgileriniz tarafımızca bilinmemektedir.',
-                    textAlign: TextAlign.start,
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 14.0,
-                        fontFamily: 'Quicksand',
-                        fontWeight: FontWeight.w500),
+                  Container(
+                      height: 25,
+                      width: 358,
+                      margin: EdgeInsets.only(
+                        top: 20,
+                        left: 20,
+                      ),
+                      alignment: Alignment.topLeft,
+                      child: TextField(
+                        controller: cardLastUsage,
+                        decoration: InputDecoration(),
+                        onTapOutside: (event) =>
+                            FocusManager.instance.primaryFocus?.unfocus(),
+                      )),
+                  SizedBox(
+                    height: 10,
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-              ],
+                  Container(
+                    height: 72,
+                    width: 323,
+                    margin: EdgeInsets.only(
+                      top: 20,
+                      left: 20,
+                    ),
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Kart bilgileriniz Masterpass altyapısında saklanır. Kredi kartınızı tanımlanamanız halinde, telefon numaranıza ait bir MasterPass hesabı oluşturulur. Kart bilgileriniz tarafımızca bilinmemektedir.',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 12.0,
+                          fontFamily: 'Quicksand',
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    height: 30,
+                    width: 323,
+                    margin: EdgeInsets.only(
+                      top: 20,
+                      left: 20,
+                    ),
+                    alignment: Alignment.topLeft,
+                    child: Row(
+                      children: [
+                        /*Checkbox(
+                          value: false,
+                          onChanged: () {},
+                        ),*/
+                        Text(
+                          'Masterpass hesabı oluşturulmasını onaylıyorum.',
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12.0,
+                              fontFamily: 'Quicksand',
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 72,
+                    width: 323,
+                    margin: EdgeInsets.only(
+                      left: 20,
+                    ),
+                    alignment: Alignment.topLeft,
+                    child: Row(
+                      children: [
+                        /*Checkbox(
+        value: false,
+        onChanged: () {},
+      ),*/
+                        Expanded(
+                          child: Text(
+                            'Masterpass kullanıcı sözleşmesini okudum ve kabul ediyorum.',
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12.0,
+                              fontFamily: 'Quicksand',
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ));
